11ab792957871c2e96d98fd999bdf894e13c5a80
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-07-29 08:48:49 -0700

    fixed mobx and babel issues

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Store from './stores/Store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = (
+  <Provider Store={Store}>
+    <App />
+  </Provider>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(Root, document.getElementById('root'));
 serviceWorker.unregister();
